07053fcd0037e70f09daf7be0eb2793a82032554
Joel Alfaro <alfarojoela@outlook.com>
2021-09-15 08:27:42 -0700

    has random path color and direction generated.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from turtle import Turtle, Screen
+import turtle as t #this was needed for the random color.  not sure what it does
 import heroes
 import random
 
-tim = Turtle()
+tim = t.Turtle()  #same as above.  not sure of purpose.
 tim.shape("turtle")
 tim.color("blue")
+t.colormode(255)  #needed to do this so that random tuple can be generated to choose random color
+
 
 
 # timmy_the_turtle.forward(100)
@@ -67,22 +70,45 @@ tim.color("blue")
 # for _ in range(10):
 #     tim.right(36)
 #     tim.forward(100)
-colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "Wheat", "SlateGray", "SeaGreen"]
 
-def draw_shape(num_sides):
-    angle = 360/ num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
 
-#iterates through range of numbers that corresponds to sides of shape.
-#3 for triangle
-#4 for square
-#5 for pentago ... etc.
-for shape_side_n in range(3, 11):
-    tim.color(random.choice(colors))
-    draw_shape(shape_side_n)
+########################################################################################
+# colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "Wheat", "SlateGray", "SeaGreen"
+#           ,"BlueViolet", "LavenderBlush3", "LemonChiffon", "SpringGreen", "VioletRed", "WhiteSmoke"]
+# # def draw_shape(num_sides):
+# #     angle = 360/ num_sides
+# #     for _ in range(num_sides):
+# #         tim.forward(100)
+# #         tim.right(angle)
+#
+# #iterates through range of numbers that corresponds to sides of shape.
+# #3 for triangle
+# #4 for square
+# #5 for pentago ... etc.
+# for shape_side_n in range(3, 11):
+#     tim.color(random.choice(colors))
+#     draw_shape(shape_side_n)
+###########################################################################################
+#colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "Wheat", "SlateGray", "SeaGreen"
+#          ,"BlueViolet", "LavenderBlush3", "LemonChiffon", "SpringGreen", "VioletRed", "WhiteSmoke"]
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
 
+angles = [90, 180, 270, 360]
+
+tim.width(10)
+tim.speed("fastest")
+x, y = tim.position()
+
+for _ in range(300):
+    tim.setheading(random.choice(angles))
+    tim.forward(random.randint(0, 30))
+    tim.color(random_color())
+
+##################################################################
 
 
 
